9e6154057bc3a9ae54bdd0d1221432331bf575d1
shubham <grid7698@gmail.com>
2026-02-08 00:15:09 +0530

    Fix MongoDB connection

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/ResumeAnalyzer')
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("server is connected to db ")
     } catch (error) {
         console.error("❌ Database connection failed:", error.message);
